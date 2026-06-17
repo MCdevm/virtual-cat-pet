1868d4f151bf1103f73f132ffb90781e75c14415
Supaming Prombut <spreawnapa6@gmail.com>
2026-06-18 01:04:36 +0700

    clean: remove Flutter project files, keep only web game (HTML/CSS/JS)

--- sw-clear.js
+++
@@ -1,9 +0,0 @@
-// Unregister all old service workers (e.g. leftover Flutter SW)
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
-  );
-});
